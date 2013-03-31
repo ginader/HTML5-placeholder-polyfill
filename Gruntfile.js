c21b00fa4fd6a3e8df1ae29a6813c713b5f45b78
Dirk Ginader <dirk@ginader.com>
2013-03-31 16:53:01 -0700

    removed the markdown task temporally because it seems broken (maybe a Grunt 0.4.0 issue?)

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,18 +63,20 @@ module.exports = function(grunt) {
         }
       }
     },
-    markdown: {
-      all: {
-        files: ['readme.markdown','version-history.markdown'],
-        template: 'web/template.html',
-        dest: 'web',
-        options: {
-          gfm: true,
-          codeLines: {
-            before: '<span>',
-            after: '</span>'
-          }
-        }
+    // markdown: {
+    //   all: {
+    //     files: ['readme.markdown','version-history.markdown'],
+    //     template: 'web/template.html',
+    //     dest: 'web',
+    //     options: {
+    //       gfm: true,
+    //       codeLines: {
+    //         before: '<span>',
+    //         after: '</span>'
+    //       }
+    //     }
+    //   }
+    // },
     watch: {
       gruntfile: {
         files: '<%= jshint.files %>',
@@ -84,7 +86,9 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'concat', 'markdown']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'concat'
+    //, 'markdown'
+  ]);
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
